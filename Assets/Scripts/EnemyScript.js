31357aa80b3be9b87a21ed8101e31384014f2198
Matt Lamb <lambmat1@msu.edu>
2017-05-01 03:23:21 -0400

    First commit

---
+++ Assets/Scripts/EnemyScript.js
@@ -0,0 +1,12 @@
+﻿var EnemyHealth : int = 10;
+
+function DeductPoints(DamageAmount : int) {
+	EnemyHealth -= DamageAmount;
+}
+
+function Update() {
+	if(EnemyHealth <= 0) {
+		Destroy(gameObject);
+	}
+}
+	
---
+++ Assets/Scripts/HandGunDamage.js
@@ -0,0 +1,16 @@
+﻿var DamageAmount : int = 5;
+var TargetDistance : float;
+var AllowedRange : float = 15;
+
+function Update () {
+	if(Input.GetButtonDown("Fire1")) {
+
+		var Shot : RaycastHit;
+			if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), Shot)) {
+				TargetDistance = Shot.distance;
+				if(TargetDistance < AllowedRange) {
+					Shot.transform.SendMessage("DeductPoints", DamageAmount);
+				}
+			}
+		}
+	}

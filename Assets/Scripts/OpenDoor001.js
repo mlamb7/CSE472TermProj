d947c8cc16efa5d51c45e2ad0e4becf6842975ba
Matt Lamb <lambmat1@msu.edu>
2017-05-01 21:21:12 -0400

    missed some stuff from last commit

---
+++ Assets/Scripts/OpenDoor001.js
@@ -0,0 +1,18 @@
+﻿import UnityEngine.UI;
+
+var TextDisplay : GameObject;
+var TheDistance : float = PlayerCasting.DistanceFromTarget;
+
+function Update() {
+	TheDistance = PlayerCasting.DistanceFromTarget;
+}
+
+function OnMouseOver() {
+	if(TheDistance <= 2) {
+		TextDisplay.GetComponent.<Text>().text = "Activate Switch";
+	}
+}
+
+function OnMouseExit() {
+	TextDisplay.GetComponent.<Text>().text = "";
+}
